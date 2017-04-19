958ec9227004f6cbdcd87734036b7125197c3a30
Kalman Jantner <jantner@gmail.com>
2017-04-19 16:56:36 +0200

    Initial change to implement same approach like in Mobius for Sparkjl

--- src/init.jl
+++ src/init.jl
@@ -4,8 +4,9 @@ function init()
     sparkjlassembly = joinpath(dirname(@__FILE__), "..", "jvm", "sparkjl", "target", "sparkjl-0.1-assembly.jar")
     classpath = @static is_windows() ? "$envcp;$sparkjlassembly" : "$envcp:$sparkjlassembly"
     try
+        print("JVM starting from init.jl")
         # prevent exceptions in REPL on code reloading
-        JavaCall.init(["-ea", "-Xmx1024M", "-Djava.class.path=$classpath"])
+        JavaCall.init(["-ea", "-Xmx1024M", "-Djava.class.path=$classpath -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=4000"])
     end
 end
 
