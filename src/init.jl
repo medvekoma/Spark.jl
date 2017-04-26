5fb300009d53cb6ddcf08b4aaa12172446bf18d7
Kalman Jantner <jantner@gmail.com>
2017-04-26 13:08:18 +0200

    add HADOOP_CONF_DIR and YARN_CONF_DIR to the classpath to make available core-site.xml for spark context

--- src/init.jl
+++ src/init.jl
@@ -1,8 +1,11 @@
 
 function init()
     envcp = get(ENV, "CLASSPATH", "")
+    hadoopConfDir = get(ENV, "HADOOP_CONF_DIR", "")
+    yarnConfDir = get(ENV, "YARN_CONF_DIR", "")
     sparkjlassembly = joinpath(dirname(@__FILE__), "..", "jvm", "sparkjl", "target", "sparkjl-0.1-assembly.jar")
     classpath = @static is_windows() ? "$envcp;$sparkjlassembly" : "$envcp:$sparkjlassembly"
+    classpath = "$classpath:$hadoopConfDir:$yarnConfDir"
     try
         print("JVM starting from init.jl")
         # prevent exceptions in REPL on code reloading
