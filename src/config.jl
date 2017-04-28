cc1b12708e38c90df0586591b5f7ad3c284b67fc
Kalman Jantner <jantner@gmail.com>
2017-04-28 15:04:21 +0200

    fine tune yarn/spark params to make cartesian test run

--- src/config.jl
+++ src/config.jl
@@ -65,4 +65,16 @@ end
 
 function setdeploy(conf::SparkConf, deploymode::AbstractString)
     jcall(conf.jconf, "set", JSparkConf, (JString, JString), "deploy-mode", deploymode)
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.executor.instances", "1")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.dynamicAllocation.enabled", "true")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.dynamicAllocation.minExecutors", "2")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.dynamicAllocation.maxExecutors", "40")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.dynamicAllocation.initialExecutors", "2")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.shuffle.service.enabled", "true")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.driver.cores", "1")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.executor.cores", "1")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.executor.memory", "2048M")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "spark.yarn.executor.memoryOverhead", "2048M")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "yarn.scheduler.maximum-allocation-mb", "3096")
+    jcall(conf.jconf, "set", JSparkConf, (JString, JString), "yarn.nodemanager.resource.memory-mb", "3096")
 end
